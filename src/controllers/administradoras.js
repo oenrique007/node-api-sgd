5871182dc074ab69a6631bcafc8c0621e8725c5e
Oscar Julio Estrada <oenrique@gmail.com>
2021-08-08 20:42:05 -0500

    Treceavo Commit

--- src/controllers/administradoras.js
+++ src/controllers/administradoras.js
@@ -9,7 +9,7 @@ var NombreTabla = "Administradoras";
 //exportar modulo
 module.exports = [
     {
-        method: 'GET',
+        method: 'POST',
         path: `${pathDef}/todos`,
         description: 'Consultar todas las administradoras',
         handler: function (req, res) {
--- src/controllers/unidadesfuncionales.js
+++ src/controllers/unidadesfuncionales.js
@@ -9,7 +9,7 @@ var NombreTabla = "UnidadesFuncionales";
 //exportar modulo
 module.exports = [
     {
-        method: 'GET',
+        method: 'POST',
         path: `${pathDef}/todos`,
         description: 'Consultar todas las unidades funcionales',
         handler: function (req, res) {
